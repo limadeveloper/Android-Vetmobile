424566f2ef5f074d51bc09d8a57d417d2dfa9e56
John Lima <john.limadeveloper@gmail.com>
2017-10-21 12:20:12 -0200

    Cadastro
    Ajusta tela de cadastro

--- app/src/main/java/android/vetmobile/com/vet/RegisterActivity.java
+++ app/src/main/java/android/vetmobile/com/vet/RegisterActivity.java
@@ -1,5 +1,6 @@
 package android.vetmobile.com.vet;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,27 +19,50 @@ public class RegisterActivity extends AppCompatActivity implements DatePickerDia
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        dateButton = (Button) findViewById(R.id.birthday_button_id);
+        dateButton = findViewById(R.id.birthday_button_id);
 
         dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Calendar now = Calendar.getInstance();
-                DatePickerDialog dpd = DatePickerDialog.newInstance(
-                        RegisterActivity.this,
-                        now.get(Calendar.YEAR),
-                        now.get(Calendar.MONTH),
-                        now.get(Calendar.DAY_OF_MONTH)
-                );
-                dpd.show(getFragmentManager(), "Datepickerdialog");
+                createDatePicker();
             }
         });
+
+        setOrientation();
     }
 
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
         String date = dayOfMonth + "/" + (++monthOfYear) + "/" + year;
         dateButton.setText(date.toString());
+    }
+
+    public void createDatePicker() {
+
+        Calendar now = Calendar.getInstance();
+        String datePickerTitle = getResources().getString(R.string.selectBirthday);
+
+        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
+                RegisterActivity.this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+
+        datePickerDialog.setVersion(DatePickerDialog.Version.VERSION_1);
+        datePickerDialog.setThemeDark(true); //set dark them for dialog?
+        datePickerDialog.vibrate(true); //vibrate on choosing date?
+        datePickerDialog.dismissOnPause(true); //dismiss dialog when onPause() called?
+        datePickerDialog.showYearPickerFirst(true); //choose year first?
+        datePickerDialog.setTitle(datePickerTitle); //dialog title
+        datePickerDialog.show(getFragmentManager(), "Datepickerdialog"); //show dialog
+    }
+
+    private void setOrientation() {
+        if (DeviceSettings.isTablet(getWindowManager())) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }else{
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
 }
